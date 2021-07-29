8b52dc17db64da37ddd0ef4f4b2f208d34dada18
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-07-29 17:24:53 +0700

    Data binding-form login-user-employee

---
+++ databinding-form-tutorial/src/main/java/com/amela/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.amela.controller;
+
+import com.amela.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        model.addAttribute("id", employee.getId());
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        return "employee/info";
+    }
+}
---
+++ databinding-form-tutorial/src/main/webapp/WEB-INF/view/user/info.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>User Information</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<h3>Static data build-in UserDAO</h3>
+<h3>Account: ${user.account}</h3>
+<h3>Name: ${user.name}</h3>
+<h3>Email: ${user.email}</h3>
+<h3>Age: ${user.age}</h3>
+<h4><a href="/home"><button type="submit">Return Home</button></a></h4>
+</body>
+</html>

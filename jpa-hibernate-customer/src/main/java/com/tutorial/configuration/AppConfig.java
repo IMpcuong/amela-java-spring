f54aed21170ca1803117bd2d74cf0de22e973ff8
IMpossible <58588543+CuongTrinh1280@users.noreply.github.com>
2021-07-30 17:58:43 +0700

    Update AppConfig.java

--- jpa-hibernate-customer/src/main/java/com/tutorial/configuration/AppConfig.java
+++ jpa-hibernate-customer/src/main/java/com/tutorial/configuration/AppConfig.java
@@ -96,7 +96,7 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3307/jpa-tutorial");
         dataSource.setUsername("root");
-        dataSource.setPassword("cuongthan1280");
+        dataSource.setPassword("*****");
         return dataSource;
     }
 
